cfc019754bd0add78b599cb454f30f45f55f377b
Enaium <32991121+Enaium@users.noreply.github.com>
2020-03-16 18:49:05 +0800

    add ClickGUI

--- src/main/java/cn/enaium/foxbase/gui/clickgui/setting/ValueSettingElement.java
+++ src/main/java/cn/enaium/foxbase/gui/clickgui/setting/ValueSettingElement.java
@@ -2,7 +2,10 @@ package cn.enaium.foxbase.gui.clickgui.setting;
 
 import cn.enaium.foxbase.setting.Setting;
 import cn.enaium.foxbase.utils.ColorUtils;
+import cn.enaium.foxbase.utils.FontUtils;
 import cn.enaium.foxbase.utils.Render2D;
+
+import java.awt.*;
 
 public class ValueSettingElement extends SettingElement {
 
@@ -16,32 +19,38 @@ public class ValueSettingElement extends SettingElement {
     @Override
     public void render(int mouseX, int mouseY, float delta, double x, double y, double width, double height) {
         super.render(mouseX, mouseY, delta, x, y, width, height);
-        double Width = width / 2;
-        double Height = height;
-        int color = ColorUtils.CHECK_BG;
-        if (this.setting.isToggle()) color = ColorUtils.CHECK_TOGGLE;
-        if (this.addHovered) color = ColorUtils.SELECT;
-        if (this.removeHovered) color = ColorUtils.SELECT;
-        Render2D.drawRectWH(x + Width + 2, y + 2, Height - 4, height - 4, color);
-        Render2D.drawRectWH(x + Width + 2 + Width, y + 2, Height - 4, height - 4, color);
+        this.addHovered = Render2D.isHovered(mouseX, mouseY, x + width + 2, y + 2, height / 2 - 4, height - 4);
+        this.removeHovered = Render2D.isHovered(mouseX, mouseY, x + width + 2 + 8, y + 2, height / 2 - 4, height - 4);
+        Render2D.drawRectWH(x + width + 2, y + 2, height / 2 - 4, height - 4, this.addHovered ? ColorUtils.BG : ColorUtils.SELECT);
+        Render2D.drawRectWH(x + width + 2 + 8, y + 2, height / 2 - 4, height - 4, this.removeHovered ? ColorUtils.BG : ColorUtils.SELECT);
+        FontUtils.drawHVCenteredString("+", x + width + 2 + width / 2, y + 2 + height / 2, Color.WHITE.getRGB());
+        FontUtils.drawHVCenteredString("-", x + width + 2 + 8 + width / 2, y + 2 + height / 2, Color.WHITE.getRGB());
     }
 
     @Override
     public void mouseClicked(double mouseX, double mouseY, int button) {
         if (this.addHovered && button == 0) {
-            if (this.setting.isValueInt())
-                this.setting.setCurrentValueInt(this.setting.getCurrentValueInt() + 1);
-            else if (this.setting.isValueFloat())
-                this.setting.setCurrentValueFloat(this.setting.getCurrentValueFloat() + 0.1F);
-            else if (this.setting.isValueDouble())
-                this.setting.setCurrentValueDouble(this.setting.getCurrentValueDouble() + 0.1D);
+            if (this.setting.isValueInt()) {
+                if (this.setting.getCurrentValueInt() <= this.setting.getMaxValueInt())
+                    this.setting.setCurrentValueInt(this.setting.getCurrentValueInt() + 1);
+            } else if (this.setting.isValueFloat()) {
+                if (this.setting.getCurrentValueFloat() <= this.setting.getMaxValueFloat())
+                    this.setting.setCurrentValueFloat(this.setting.getCurrentValueFloat() + 0.1F);
+            } else if (this.setting.isValueDouble()) {
+                if (this.setting.getCurrentValueDouble() <= this.setting.getMaxValueDouble())
+                    this.setting.setCurrentValueDouble(this.setting.getCurrentValueDouble() + 0.1D);
+            }
         } else if (this.removeHovered && button == 0) {
-            if (this.setting.isValueInt())
-                this.setting.setCurrentValueInt(this.setting.getCurrentValueInt() - 1);
-            else if (this.setting.isValueFloat())
-                this.setting.setCurrentValueFloat(this.setting.getCurrentValueFloat() - 0.1F);
-            else if (this.setting.isValueDouble())
-                this.setting.setCurrentValueDouble(this.setting.getCurrentValueDouble() - 0.1D);
+            if (this.setting.isValueInt()) {
+                if (this.setting.getCurrentValueInt() >= this.setting.getMinValueInt())
+                    this.setting.setCurrentValueInt(this.setting.getCurrentValueInt() - 1);
+            } else if (this.setting.isValueFloat()) {
+                if (this.setting.getCurrentValueFloat() >= this.setting.getMinValueFloat())
+                    this.setting.setCurrentValueFloat(this.setting.getCurrentValueFloat() - 0.1F);
+            } else if (this.setting.isValueDouble()) {
+                if (this.setting.getCurrentValueDouble() >= this.setting.getMinValueDouble())
+                    this.setting.setCurrentValueDouble(this.setting.getCurrentValueDouble() - 0.1D);
+            }
         }
         super.mouseClicked(mouseX, mouseY, button);
     }
