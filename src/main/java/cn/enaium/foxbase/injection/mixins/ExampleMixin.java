5d0bf5347acb584e8cc874ca851d8a48e298953e
Enaium <32991121+Enaium@users.noreply.github.com>
2020-03-24 13:29:26 +0800

    Del ()V

--- src/main/java/cn/enaium/foxbase/injection/mixins/ExampleMixin.java
+++ src/main/java/cn/enaium/foxbase/injection/mixins/ExampleMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  */
 @Mixin(Minecraft.class)
 public class ExampleMixin {
-    @Inject(at = @At("RETURN"), method = "startGame()V")
+    @Inject(at = @At("RETURN"), method = "startGame")
     private void startGame(CallbackInfo info) {
         FoxBase.instance.Start();
     }
